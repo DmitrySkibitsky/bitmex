8f8bf1697c17efb7c52c4df9d95f5984947a992c
Dmitry Skibitsky <dmitryskibitskiy@gmail.com>
2021-03-03 12:55:54 +0200

    upd 404 page

--- src/views/errors/404.vue
+++ src/views/errors/404.vue
@@ -1,5 +1,22 @@
 <template>
-  <p>404</p>
+  <v-container>
+    <v-layout
+      row
+      wrap
+    >
+      <v-flex xs12>
+        <v-card
+          class="text-center"
+          color="transparent"
+          flat
+        >
+          <div class="display-1 mt-10">
+            Page not found.
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 <script>
 export default {
